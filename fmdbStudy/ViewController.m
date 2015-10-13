aceca3dc9e3f65f988d69f194d5b0e7992b53286
Carter Chang <carter@yahoo-inc.com>
2015-10-13 17:32:56 +0800

    add schema for history and favoriteItem

--- fmdbStudy/ViewController.m
+++ fmdbStudy/ViewController.m
@@ -37,11 +37,34 @@ FMDatabase *database;
         NSLog(@"DB Can't Open");
     }
     
+    
+    NSString *productSchema = @"id TEXT PRIMARY KEY, image TEXT, title TEXT, market TEXT, desc TEXT, price INTEGER, url TEXT";
+    
     //Create Table
-    [database executeUpdate:@"CREATE TABLE IF NOT EXISTS favoriteList(id INTEGER primary key, name TEXT, ts TIMESTAMP)"];
-
+//    [database executeUpdate:@"CREATE TABLE IF NOT EXISTS favoriteList(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, ts TIMESTAMP)"];
+    
+    [self tableCreator:@"favoriteList" schema:@"id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, ts TIMESTAMP"];
+    [self tableCreator:@"favoriteItem" schema:[NSString stringWithFormat:@"%@, %@", productSchema, @"listId INTEGER"]];
+    [self tableCreator:@"viewHistory" schema:[NSString stringWithFormat:@"%@, %@", productSchema, @"ts TIMESTAMP"]];
+    [self tableCreator:@"searchHistory" schema:@"id INTEGER PRIMARY KEY AUTOINCREMENT, keyword TEXT"];
     
     [database close];
+}
+
+- (void) tableCreator:(NSString*)tableName schema:(NSString*)schema {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docsPath = [paths objectAtIndex:0];
+    NSString *path = [docsPath stringByAppendingPathComponent:@"shoppingGuide.sqlite"];
+    
+    NSLog(@"path is %@",docsPath);
+    database = [FMDatabase databaseWithPath:path];
+    
+    if(![database open]){
+        NSLog(@"DB Can't Open");
+    }
+
+    NSString *sqlCreateCmd = @"CREATE TABLE IF NOT EXISTS";
+    [database executeUpdate:[NSString stringWithFormat:@"%@ %@(%@)",sqlCreateCmd, tableName, schema]];
 }
 
 - (IBAction)onDeleteTable:(id)sender {
@@ -55,9 +78,11 @@ FMDatabase *database;
     }
     
     [database executeUpdate:@"DROP TABLE favoriteList"];
+    [database executeUpdate:@"DROP TABLE favoriteItem"];
+    [database executeUpdate:@"DROP TABLE viewHistory"];
+    [database executeUpdate:@"DROP TABLE searchHistory"];
     
     [database close];
-
 }
 
 
@@ -74,9 +99,9 @@ FMDatabase *database;
     
     NSNumber *timestamp = [NSNumber numberWithInteger:[[NSDate date] timeIntervalSince1970]];
     NSString *name = @"星期天記得買2";
-    NSInteger favId = [NSNumber numberWithInt:20];
    
-    [database executeUpdate:@"INSERT INTO favoriteList(id,name,ts) VALUES (?, ?, ?)", favId, name, timestamp];
+    [database executeUpdate:@"INSERT INTO favoriteList(name,ts) VALUES ( ?, ?)", name, timestamp];
+
     [database close];
 }
 
@@ -93,7 +118,7 @@ FMDatabase *database;
         NSLog(@"DB Can't Open");
     }
     
-    [database executeUpdate:@"DELETE FROM favoriteList WHERE id=20"];
+    [database executeUpdate:@"DELETE FROM favoriteList WHERE id=1"];
     
     [database close];
 }
@@ -112,7 +137,7 @@ FMDatabase *database;
 
     
     NSNumber *timestamp = [NSNumber numberWithInteger:[[NSDate date] timeIntervalSince1970]];
-    NSInteger favId = [NSNumber numberWithInt:20];
+    NSInteger favId = [NSNumber numberWithInt:1];
     
     [database executeUpdate:@"UPDATE favoriteList SET ts=? WHERE id=?", timestamp, favId];
     
